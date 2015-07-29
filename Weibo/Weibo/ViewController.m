22742382b84f1c768802c484bb8e2297f634700e
stone <hfx24395481@163.com>
2015-07-29 18:09:17 +0800

     刚刚才添加的程序

---
+++ Weibo/Weibo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Weibo
+//
+//  Created by carl on 15/7/29.
+//  Copyright (c) 2015年 Jeff He. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    NSLog(@"第一次输出的文件");
+}
+
+@end
