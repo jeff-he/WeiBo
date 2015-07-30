c3ee590c4e5ef64b641cefe48f1ec37737ba4207
stone <hfx24395481@163.com>
2015-07-30 15:11:08 +0800

    add new Person

---
+++ Weibo/Weibo/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  Weibo
+//
+//  Created by jeff on 15/7/30.
+//  Copyright (c) 2015年 Jeff He. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+@property (nonatomic, copy) NSString *name;
+@end
---
+++ Weibo/Weibo/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  Weibo
+//
+//  Created by jeff on 15/7/30.
+//  Copyright (c) 2015年 Jeff He. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
